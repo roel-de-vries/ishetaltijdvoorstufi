c3d914b1b33ab071bfc4775498e854d525f0299e
Roel de Vries <roel.de.vries@quicknet.nl>
2020-02-26 16:40:27 +0100

    Removed attempt to predict dates. Started developing solution which just downloads dates

--- js/site.js
+++ js/site.js
@@ -1,45 +1,57 @@
-// A stufi-transaction will be made on the 24th, unless that date falls in the weekend which in that case the transaction will be made on the Friday before.
-// 24 December is also skipped: if it falls on Monday or the weekend the transaction will be made on the Friday before. Otherwise the day before the 24th.
+// Retrieves the payment dates from duo.nl and caches the result. Repeat if the dates from the cache are in the past.
 
-var date = new Date(),
+const duoPage = 'https://duo.nl/particulier/footer-engels/service/payment-dates.jsp';
+
+$.ajax({
+    crossOrigin: true,
+    url: duoPage,
+    success: function(data) {
+        console.log(data);
+    }
+});
+
+var dateToday = moment().startOf('day'),
 daysTillStufi = 0,
-isItToidElement = document.getElementById('isItToid'),
+isItTimeElement = document.getElementById('isItTime'),
 moreInfoElement = document.getElementById('moreInfo'),
-stufiDate = GetStufiDate();
+paymentDates;
 
-// Check if it's already stufi time
-if(date.getDate() === stufiDate.getDate()) {
-    isItToidElement.innerHTML = 'Ja!';
-    isItToidElement.style.color = '#2ecc71';
-    moreInfoElement.innerHTML = 'Dank u ome Duo!'
-}
-else {
-    // Check if we've surpassed this months stufi date
-    if(date.getDate() > stufiDate.getDate()) {
-        stufiDate = GetStufiDate(1);
+StartApp();
+
+function StartApp() {
+    if(!UpdateLocalStorage()){
+        return;
     }
+    
+    // Check if it's already stufi time
+    if(dateToday.getDate() === stufiDate.getDate()) {
+        isItTimeElement.innerHTML = 'Ja!';
+        isItTimeElement.style.color = '#2ecc71';
+        moreInfoElement.innerHTML = 'Dank u ome Duo!'
+    }
+    else {
+        daysTillStufi = CalculateDaysDifference(dateToday, stufiDate);
 
-    daysTillStufi = CalculateDaysDifference(date, stufiDate);
+        isItTimeElement.innerHTML = 'Nee!';
+        isItTimeElement.style.color = '#e74c3c';
 
-    isItToidElement.innerHTML = 'Nee!';
-    isItToidElement.style.color = '#e74c3c';
-
-    switch (daysTillStufi) {
-        case 0:
-            moreInfoElement.innerHTML = 'Nog een paar uur te gaan tot de ' + stufiDate.getDate() + 'e , hou vol!';
-            break;
-        case 1:
-            moreInfoElement.innerHTML = 'Nog één dag te gaan tot de ' + stufiDate.getDate() + 'e, hou vol!';
-            break;
-        default:
-            moreInfoElement.innerHTML = 'Nog ' + daysTillStufi + ' dagen te gaan tot de ' + stufiDate.getDate() + 'e, hou vol!';
-            break;
+        switch (daysTillStufi) {
+            case 0:
+                moreInfoElement.innerHTML = 'Nog een paar uur te gaan tot de ' + stufiDate.getDate() + 'e , hou vol!';
+                break;
+            case 1:
+                moreInfoElement.innerHTML = 'Nog één dag te gaan tot de ' + stufiDate.getDate() + 'e, hou vol!';
+                break;
+            default:
+                moreInfoElement.innerHTML = 'Nog ' + daysTillStufi + ' dagen te gaan tot de ' + stufiDate.getDate() + 'e, hou vol!';
+                break;
+        }
     }
 }
 
-function GetStufiDate(monthOffset = 0) {
+function GetStufiDate() {
     var countDownDay = 24,
-        dateHelper = new Date(date.getFullYear(), date.getMonth(), countDownDay);
+        dateHelper = new Date(dateToday.getFullYear(), dateToday.getMonth(), countDownDay);
 
     // Check if a date with a month offset needs to be calculated
     if(monthOffset) {
@@ -72,7 +84,84 @@ function GetStufiDate(monthOffset = 0) {
     return new Date(dateHelper.getFullYear(), dateHelper.getMonth(), countDownDay);
 }
 
+function UpdateLocalStorage() {
+    if (localStorage.getItem('ihatvs') === null) {
+        PopulateDates();
+        return false;
+    }
+    
+    let cachedDates = ParseLocalStorageToDates();
+
+    // Chech if the cached dates are in the past.
+    if(moment(cachedDates[cachedDates.length - 1]).isBefore(dateToday)){
+        PopulateDates();
+        cachedDates = ParseLocalStorageToDates();
+    }
+
+    // Remove all dates in the past since duo doesn't do this after each date has passed.
+    for(let i = 0; i < cachedDates.length; i++) {
+        if(moment(cachedDates[i]).isBefore(dateToday)){
+            cachedDates.shift();
+            i--;
+            continue;
+        }
+
+        break;
+    }
+
+    paymentDates = cachedDates;
+
+    return true;
+}
+
+// Gets json dates from the local storage and parses them back to objects.
+function ParseLocalStorageToDates() {
+    let datesJson = JSON.parse(localStorage.getItem('ihatvs')),
+    dateObjects = [];
+
+    datesJson.each(function(index, date) {
+        dateObjects.push(moment(date, 'DD-MM-YYYY'));
+    })
+
+    return dateObjects;
+}
+
+// Fills the local storage with the dates retrieved from duo.
+function PopulateDates() {
+    // Uses google apps script as proxy to retrieve the html from DUO.
+    $.ajax({
+        crossOrigin: true,
+        url: duoPage,
+        success: function(data) {
+            let $duoHtml = $(data),
+            payYearSelector = '#payment-dates-' + dateToday.year(),
+            $paymentDateList = $duoHtml.find(payYearSelector).siblings('ul').find('li');
+
+            // Get the payment dates
+            let dateObjects = CreatePaymentDates($paymentDateList);
+
+            localStorage.setItem('ihatvs', JSON.stringify(dateObjects));
+
+            StartApp();
+        }
+    });
+}
+
+// Duo provides a list with string dates, this method converts those to date objects.
+function CreatePaymentDates($paymentDateStrings){
+    let convertedDates = [],
+    currentYear = dateToday.getFullYear;
+
+    $paymentDateStrings.each(function (index, date) {
+        let paymentDate = moment(date + " " + currentYear, "DD MMMM YYYY");
+        convertedDates.push(paymentDate);
+    });
+
+    return convertedDates;
+}
+
 function CalculateDaysDifference(dateOne, dateTwo) {
     var oneDay = 24*60*60*1000;
     return Math.round(Math.abs(dateOne.getTime() - dateTwo.getTime()) / (oneDay));
 }
+
